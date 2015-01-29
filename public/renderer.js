514766bf21ea51264e057dfea35c9ec30187dfc8
Ben Delarre <ben@delarre.net>
2015-01-29 12:56:41 -0800

    Fixed scaling in renderer.

--- public/renderer.js
+++ public/renderer.js
@@ -3,16 +3,16 @@
 
 	var Viewer = function(containerId) {
 		this.containerId = containerId;
+		this.container = document.getElementById(containerId);
 		this.init();
 	}
 
 	Viewer.prototype = {
 		init: function() {
-			this.viewWidth = 640;
-			this.viewHeight = 380;
+			this.viewWidth = this.container.clientWidth;
+			this.viewHeight = this.container.clientHeight;
 			this.pixelWidth = 54;
 			this.pixelHeight = 32;
-			this.pixelSize = 16;
 
 			var renderer = new PIXI.autoDetectRenderer(this.viewWidth, this.viewHeight);
 			renderer.view.className = "renderer-view";
@@ -29,19 +29,53 @@
 
 			this.stage = stage;
 
+			this.pixelContainer = new PIXI.DisplayObjectContainer();
+			stage.addChild(this.pixelContainer);
+
 			this.initPixels();
 
 			requestAnimationFrame(this.animate.bind(this));
 
 			this.startSocket();
 		},
+
+		
+		resize: function() {
+			this.viewWidth = this.container.clientWidth;
+			this.viewHeight = this.container.clientHeight;
+
+			this.renderer.resize(this.viewWidth, this.viewHeight);
+
+			var renderWidth = 640, renderHeight = 380;
+
+			var pixelSpacing = Math.min(Math.floor(renderWidth/(this.pixelWidth-1)), Math.floor(renderHeight/(this.pixelHeight-1)));
+
+			// ideal rendering is at 640x380
+			// figure out scale ratio for this new size
+			var scaleX = 1 / (renderWidth / this.viewWidth),
+				scaleY = 1 / (renderHeight / this.viewHeight);
+
+			var minScale = Math.min(scaleX,scaleY);
+			this.pixelContainer.scale = new PIXI.Point(minScale,minScale);
+
+			var pixelXOffset = (renderWidth - (pixelSpacing*(this.pixelWidth-1))) / 2,
+				pixelYOffset = (renderHeight - (pixelSpacing*(this.pixelHeight-1))) / 2;
+
+			// create all the pixels
+			for (var x=0; x < this.pixelWidth; x++) {
+				for (var y=0; y < this.pixelHeight; y++) {
+					pixel = this.pixels[x][y];
+					pixel.position.x = pixelXOffset + (pixelSpacing * x);
+					pixel.position.y = pixelYOffset + (pixelSpacing * y);
+				}
+			}
+
+			this.pixelXOffset = pixelXOffset;
+			this.pixelYOffset = pixelYOffset;
+			this.pixelSpacing = pixelSpacing;
+		},
 		initPixels: function() {
 			var pixels = [];
-
-			var pixelSpacing = Math.min(Math.floor(this.viewWidth/this.pixelWidth), Math.floor(this.viewHeight/this.pixelHeight));
-
-			var pixelXOffset = (this.viewWidth - (pixelSpacing*(this.pixelWidth-1))) / 2,
-				pixelYOffset = (this.viewHeight - (pixelSpacing*(this.pixelHeight-1))) / 2;
 
 			// create all the pixels
 			for (var x=0; x < this.pixelWidth; x++) {
@@ -49,20 +83,15 @@
 				for (var y=0; y < this.pixelHeight; y++) {
 					var pixel = new PIXI.Sprite.fromImage("/public/point.png");
 					pixels[x][y] = pixel;
-					pixel.position.x = pixelXOffset + (pixelSpacing * x);
-					pixel.position.y = pixelYOffset + (pixelSpacing * y);
 					pixel.blendMode = PIXI.blendModes.ADD;
 					pixel.anchor.x = 0.5;
 					pixel.anchor.y = 0.5;
 
-					this.stage.addChild(pixel);
+					this.pixelContainer.addChild(pixel);
 				}
 			}
-
-			this.pixelXOffset = pixelXOffset;
-			this.pixelYOffset = pixelYOffset;
-			this.pixelSpacing = pixelSpacing;
 			this.pixels = pixels;
+			this.resize();
 		},
 		updatePixels: function(data) {
 			var pixels = this.pixels;
@@ -106,5 +135,6 @@
 
 	window.Viewer = Viewer;
 
-	window.view = new Viewer();
+	window.view = new Viewer("render");
 })();
+
