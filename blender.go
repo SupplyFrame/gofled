eabdd39eb07fbef2c3637f5591cdbb04e2988927
Ben Delarre <ben@delarre.net>
2015-04-08 12:46:47 -0700

    Switched to 100% brightness by default, updated LEDscape.

--- blender.go
+++ blender.go
@@ -263,7 +263,7 @@ func NewBlender(numLEDs int, broker *Broker) *Blender {
 		data1: make([]byte, numLEDs*3),
 		data2: make([]byte, numLEDs*3),
 		framenum: 0,
-		brightness: 0.5,
+		brightness: 1.0,
 		commands: make(chan BlenderCommand, 60), // buffered channel
 	}
 	return v;
